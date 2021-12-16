aba928f93c281ad16820973b77102591f2445fcd
Alvaro Lopez <alvarolopez170@gmail.com>
2021-12-16 10:08:38 -0300

    Cuarto push
    No funciona borrar..

---
+++ src/main/java/controller/propuestas/BorrarAtraccionesServlet.java
@@ -0,0 +1,47 @@
+package controller.propuestas;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Atraccion;
+import services.AtraccionService;
+
+@WebServlet("/atracciones/borrar.adm")
+public class BorrarAtraccionesServlet extends HttpServlet implements Servlet {
+
+	private static final long serialVersionUID = -2821548431939808584L;
+	private AtraccionService atraccionService;
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		this.atraccionService = new AtraccionService();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Integer id = Integer.parseInt(req.getParameter("id"));
+		Atraccion atraccion = atraccionService.findById(id);
+		req.setAttribute("atraccion", atraccion);
+		atraccionService.delete(atraccion.getIdAtraccion());
+		
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/views/atracciones/atracciones.jsp");
+		dispatcher.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Integer id = Integer.parseInt(req.getParameter("id"));
+
+		atraccionService.delete(id);
+
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/TierraMediaWeb/atracciones/borrar.jsp");
+			dispatcher.forward(req, resp);
+		}
+}
